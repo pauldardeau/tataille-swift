f54f6f4c22fb0e95fe3596c769bdef07f19198e2
pauldardeau <pauldardeau@me.com>
2014-08-01 13:07:18 -0500

    Initial Commit

---
+++ tataille-swift/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  tataille-swift
+//
+//  Created by Paul Dardeau on 8/1/14.
+//  Copyright (c) 2014 SwampBits. All rights reserved.
+//
+
+import Cocoa
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+                            
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification?) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification?) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ tataille-swift/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  tataille-swift
+//
+//  Created by Paul Dardeau on 8/1/14.
+//  Copyright (c) 2014 SwampBits. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
